c45c85eaa3c5ad422d03e5183d48ff583df9166e
Ashish Menkudale <ashish.menkudale4@gmail.com>
2020-01-30 20:35:08 -0500

    Update QuoteCard.vue

--- components/cards/QuoteCard.vue
+++ components/cards/QuoteCard.vue
@@ -2,11 +2,10 @@
   <div class="card quote-card">
     <div class="card-content">
       <p class="title">
-        “Always code as if the guy who ends up maintaining your code will be a
-        violent psychopath who knows where you live.”
+        “If you torture the data long enough, it will confess.”
       </p>
       <p class="subtitle">
-        ~ Martin Golding
+        ~ Ronald Coase
       </p>
     </div>
     <footer class="card-footer">
